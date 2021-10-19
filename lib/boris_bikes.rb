0e4de52734db76a76a78f86a445e8251bbbce353
roshanabey2 <roshanabey2@gmail.com>
2021-10-19 16:19:46 +0100

    the method release_bike creates an instance of the bike class in the file boris_bikes.rb. Also changes the spec file ensure that rspec checks if the a new instance of the Bikes class is creates when release_bike method is called

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,9 +1,7 @@
 class DockingStation
     def release_bike
-        bike = Bikes.new
     end
 end
 
 class Bikes
-    def check_working
 end
---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,11 @@
+require 'boris_bikes.rb'
+describe DockingStation do
+    it "creates an instance of the DockingStationg class" do
+        expect{ docking_station = DockingStation.new }.not_to raise_error
+    end
+
+    it '"creates" a new bike when release_bike method is called' do
+        docking_station = DockingStation.new
+        expect{bike = docking_station.release_bike}.not_to raise_error
+    end
+end
